d543fea1c4b56ef992444bb74729ef07887294c2
ramiroAlvarez9 <ramiro.al.alvarez@gmail.com>
2021-11-11 21:16:27 -0300

    falta agregar funciones de SPA.

--- js/app.js
+++ js/app.js
@@ -1,105 +1,45 @@
-const productosTienda = [
-    {nombre: "Mouse", descripcion: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s", precio: 1000, imagen: "./img/mouse.jpg"},
-    {nombre: "Teclado", descripcion: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s", precio: 2000, imagen: "./img/teclado.jpg"},
-    {nombre: "Monitor", descripcion: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s", precio: 10000, imagen: "./img/monitor.jpg"}
-];
 
-const tarjetas = i => {
-    let cardDiv = document.createElement("div");
-    cardDiv.setAttribute("class", "main__card");
-    return cardDiv;
-}
-const generarImagenes = i => {
-
-    let img = document.createElement("img");
-    img.setAttribute("src", productosTienda[i].imagen)
-    img.setAttribute("class", "main__card--img main__card--element")
-    return img;
-}
-
-const titulos = i => {
-    let title = document.createElement("h1")
-    title.setAttribute("class", "main__card--title main__card--element")
-    title.innerHTML = productosTienda[i].nombre;
-    return title;
-}
-
-const descripciones = i => {
-
-    let description = document.createElement("p");
-    description.setAttribute("class", "main__card--p main__card--element")
-    description.innerHTML = productosTienda[i].descripcion;
-    return description;   
-
-}
-
-const precios = i => {
-
-    let price = document.createElement("h3");
-    price.setAttribute("class", "main__card--h3 main__card--element")
-    price.innerHTML = `${productosTienda[i].precio}$`;
-    return price;
+$.get( "./js/productos.json", data => {
     
-}
+    const guardarProductosEnCarrito =  i => {
+        $(`.button${i}`).click( () => {
 
-const botones = i => {
-    let button = document.createElement("button");
-    button.setAttribute("class", "main__card--button main__card--element")
-    button.innerHTML = "AGREGAR AL CARRITO";
-    return button;
-}
+            const productoNombre = data.productos[i].nombre;
+            const productoPrecio = data.productos[i].precio;
 
-
-const guardarProductosEnCarrito = (button, i) => {
+            let productosObject = {
+                productoNombre,
+                productoPrecio,
+            }       
     
-    button.addEventListener("click", () => {
-        
-        const productoNombre = productosTienda[i].nombre;
-        const productoPrecio = productosTienda[i].precio;
+            if(localStorage.getItem('productos') === null){
+                let productosArray = []
+                productosArray.push(productosObject)
+                localStorage.setItem('productos', JSON.stringify(productosArray))
+            }else{ 
+                let productosEnLocalStorage = JSON.parse(localStorage.getItem('productos'));
+                productosEnLocalStorage.push(productosObject);
+                localStorage.setItem('productos', JSON.stringify(productosEnLocalStorage))
+            }             
+        })
+    }
 
-        let productosObject = {
-            productoNombre,
-            productoPrecio,
-        }       
-    
-        if(localStorage.getItem('productos') === null){
-            let productosArray = []
-            productosArray.push(productosObject)
-            localStorage.setItem('productos', JSON.stringify(productosArray))
-        }else{ 
-            let productosEnLocalStorage = JSON.parse(localStorage.getItem('productos'));
-            productosEnLocalStorage.push(productosObject);
-            localStorage.setItem('productos', JSON.stringify(productosEnLocalStorage))
-        }             
-    })
-}
+        for (let i = 0; i < data.productos.length; i++) {
+            $(".main__container").append(
+                `<div class ="main__card">
+                    <img src = ${data.productos[i].imagen} class = "main__card--img main__card--element">
+                    <h1 class = "main__card--title main__card--element">${data.productos[i].nombre}</h1>
+                    <p class = "main__card--p main__card--element">${data.productos[i].descripcion}</p>
+                    <h3 class = "main__card--h3 main__card--element">${data.productos[i].precio}</h3>
+                    <button class = "main__card--button main__card--element button${i}">AGREGAR AL CARRITO</button>
+                </div>`
+            )
+            guardarProductosEnCarrito(i)
+    }
 
-const mostrarTodosLosProductos = (main,cardDiv,img,title,description,price,button) =>{
+}).fail(()=> {
+    console.log("error")
+    swal("ERROR 404", "Error en la base de datos, por favor contacte a soporte tecnico", "error");
 
-    cardDiv.appendChild(img)
-    cardDiv.appendChild(title) 
-    cardDiv.appendChild(description)   
-    cardDiv.appendChild(price)
-    cardDiv.appendChild(button)
-    main.appendChild(cardDiv)
-}
+});
 
-for (let i = 0; i < productosTienda.length; i++) {
-
-    //genero un main que contenga cada card
-    const main = document.createElement("main")
-    main.setAttribute("class", "main")
-    document.body.appendChild(main)
-
-    let cardDiv = tarjetas();
-    const img = generarImagenes(i);
-    const title = titulos(i);
-    const description = descripciones(i)
-    const price = precios(i);
-    const button = botones(i);
-
-    guardarProductosEnCarrito(button,i)
-    
-    mostrarTodosLosProductos(main,cardDiv,img,title,description,price,button)
-
-}
--- js/carrito.js
+++ js/carrito.js
@@ -1,40 +1,25 @@
+let productosEnLocalStorage =JSON.parse(localStorage.getItem('productos'));
 
+for (let i = 0 ; i < productosEnLocalStorage.length;i++){
 
+    $(".main").append(  `
+    <div class ="productos__container">
+            <h6 class ="productos__container--nombre">${productosEnLocalStorage[i].productoNombre}</h6>
+                <div class ="productos__container--precio" >${productosEnLocalStorage[i].productoPrecio}$</div>
+                <a href = "carrito.html" class = "productos__container--delete deleteButton${i}">Eliminar ${productosEnLocalStorage[i].productoNombre} del carrito</button>
+            </div>
+    </div>`
+    )
+    $(`.deleteButton${i}`).click(() => {
+        
+        if(productosEnLocalStorage[i].productoNombre === productosEnLocalStorage[i].productoNombre ){
 
-const mostrarProductosDelCarrito = () => {
-    let productosEnLocalStorage =JSON.parse(localStorage.getItem('productos'));
-    const productosContainer = document.querySelector('.main');
-    console.log(productosContainer)
-    for(let i = 0; i < productosEnLocalStorage.length; i++){
-    
-        let nombre = productosEnLocalStorage[i].productoNombre;
-        let precio = productosEnLocalStorage[i].productoPrecio;
-    
-        productosContainer.innerHTML += `
-
-            <div class ="productos__container">
-
-                    <h6 class ="productos__container--nombre">${nombre}</h6>
-                        <div class ="productos__container--precio" >${precio}$</div>
-                        <a href = "/carrito.html"class = "productos__container--delete" onclick = "eliminarProducto('${nombre}')">Eliminar ${nombre} del carrito</a>
-                    </div>
-            
-            </div>`
-    }   
-}
-
-const eliminarProducto = (nombre) => {
-    let productosDelLocalStorage =JSON.parse(localStorage.getItem('productos'));
-
-    for(let i = 0; i < productosDelLocalStorage.length ; i++){
-
-        if(productosDelLocalStorage[i].productoNombre === nombre ){
-
-            productosDelLocalStorage.splice(i,1);
+            productosEnLocalStorage.splice(i,1);
 
         }
-    }
-    localStorage.setItem('productos', JSON.stringify(productosDelLocalStorage));    
+        localStorage.setItem('productos', JSON.stringify(productosEnLocalStorage));
+    })
+
+    
 }
 
-mostrarProductosDelCarrito();
